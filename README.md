de1e4ff2c4afd4ebcac433b5ad34843c62b0eb8d
Amjed Mrissa <mrissamjed@gmail.com>
2024-02-12 22:17:42 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Ecowebsite
+don't forget: npm i 
+
+
+
+1. git checkout "your branch name"
+2.git pull origin main"
+3.do the work
+4. don't forget to commit and push into your "branch name"
---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const cors = require('cors')
+const PORT = 3000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(__dirname + "/public"))
+app.get('/',(req,res)=>{res.send("hello")})
+app.listen(PORT, ()=>{
+    console.log(`listening on http://localhost:${PORT}`);
+})
