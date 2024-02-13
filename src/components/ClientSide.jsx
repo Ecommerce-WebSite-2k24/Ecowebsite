06713cd49e9ccc73f60007c92aa6f7b2b230dc3b
Amjed Mrissa <mrissamjed@gmail.com>
2024-02-14 00:02:41 +0100

    starting client component + installing tailwind

---
+++ src/components/ClientSide.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ClientSide = () => {
+  return (
+    <div>
+     <section>
+
+     </section>
+     <section>
+        <h4 className='Clienth4'>Edit Your Profile</h4>
+        
+     </section>
+    </div>
+  )
+}
+
+export default ClientSide
